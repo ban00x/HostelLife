0902a678e124eded82efd611f8ed43731e2e37b9
Rana_Ahmed <foster3399@gmail.com>
2021-12-04 00:25:36 +0100

    Bootstrap, FontAwesome installed. SearchBar,  SpecificActivity and Activities component created.

--- client/react-app/src/App.js
+++ client/react-app/src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom"
+import ActivitiesPage from "./Components/ActivitiesPage";
+import SpecificActivity from "./Components/SpecificActivity";
+
 
 const App = () => {
-  return (<h1>hostellife</h1>)
+  return (
+    <>
+      <h1>Hostel Life</h1>
+      <SpecificActivity />
+      <ActivitiesPage/>
+     
+    </>
+  
+  
+  
+  )
 }
 
 export default App;
