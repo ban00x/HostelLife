e508a7b200c0f82d01570c94749a53ece048fef9
Suman Rayamajhi <meh_sams@yahoo.com>
2021-12-07 21:12:15 +0100

    final commit for react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
 import React from "react";
-import EventProfilePage from "./Components/EventProfilePage";
 import EventsPage from "./Components/EventsPage";
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Link,
-  useParams,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <h1>Hostel Life</h1>
       <BrowserRouter>
-        <Switch>
-          {/* <Route exact path="/events" component={EventsPage} /> */}
-          <Route path="/events/:category" component={EventsPage} />
-        </Switch>
+        <Routes>
+          <Route path="/events/:category" element={<EventsPage />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
