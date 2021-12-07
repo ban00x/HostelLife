7cac888bc49809782ec9b88ff437a09e9b27f4a4
Suman Rayamajhi <meh_sams@yahoo.com>
2021-12-07 12:48:00 +0100

    cmmiting to test

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ActivitiesPage from "./Components/ActivitiesPage";
 import SpecificActivity from "./Components/SpecificActivity";
 
-
 const App = () => {
+  useEffect(() => {
+    const url = "http://localhost:6000/events";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <>
       <h1>Hostel Life</h1>
       <SpecificActivity />
-      <ActivitiesPage/>
-     
+      <ActivitiesPage />
     </>
-  
-  
-  
-  )
-}
+  );
+};
 
 export default App;
