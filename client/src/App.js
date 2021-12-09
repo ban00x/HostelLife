2b72b0cf14db37c153a1f2c326c5e00ee7604056
Suman Rayamajhi <meh_sams@yahoo.com>
2021-12-09 21:54:56 +0100

    some changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import EventProfilePage from "./Components/EventProfilePage";
 import EventsPage from "./Components/EventsPage";
-// import EventsPage from "./Components/EventsPage";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
@@ -10,11 +9,11 @@ const App = () => {
       <h1>Hostel Life</h1>
       <EventProfilePage />
       <EventsPage />
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/events/:category" element={<EventsPage />} />
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
     </>
   );
 };
